a53060c84da2b95aa30a5860504ff544c0490c42
Claudia Lizeth Zamora Barahona <claudializeth_07@hotmail.com>
2021-09-27 13:29:33 -0600

    Agregando Entity Framework Code First

--- CatraSport/CatraSport.BL/ProductosBL.cs
+++ CatraSport/CatraSport.BL/ProductosBL.cs
@@ -8,29 +8,19 @@ namespace CatraSport.BL
 {
     public class ProductosBL
     {
+        Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
+        public ProductosBL()
+        {
+            _contexto = new Contexto();
+            ListadeProductos = new List<Producto>();
+        }
+
         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = " Bicicleta estática";
-            producto1.Precio = 1000;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = " Cinta de Correr";
-            producto2.Precio = 1500;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = " Bicicleta eliptica";
-            producto3.Precio = 1800;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return ListadeProductos;
         }
     }
 }
