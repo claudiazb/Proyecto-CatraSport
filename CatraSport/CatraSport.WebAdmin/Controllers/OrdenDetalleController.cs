13ea5829abb56129ac9fb319e91eb66c3d0017ab
Claudia Lizeth Zamora Barahona <claudializeth_07@hotmail.com>
2021-10-21 01:09:53 -0600

    Agregando controlador para el detalle de la orden

---
+++ CatraSport/CatraSport.WebAdmin/Controllers/OrdenDetalleController.cs
@@ -0,0 +1,28 @@
+﻿using CatraSport.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatraSport.WebAdmin.Controllers
+{
+    public class OrdenDetalleController : Controller
+    {
+        OrdenesBL _ordenBL;
+
+        public OrdenDetalleController()
+        {
+            _ordenBL = new OrdenesBL();
+           
+        }
+
+        // GET: OrdenesDetalles
+        public ActionResult Index(int id)
+        {
+            var orden = _ordenBL.ObtenerOrden(id);
+            
+            return View(orden);
+        }
+    }
+}
